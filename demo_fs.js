0e45c846dab1f4211ea70eaa55c4a558f74c0741
Sandi, ArnoldX <arnoldx.sandi@intel.com>
2018-07-04 11:48:06 -0600

    First Commit

---
+++ demo_fs.js
@@ -0,0 +1,12 @@
+var fs = require('fs');
+var http = require('http');
+var u = require('upper-case');
+
+http.createServer(function (req, res){
+    fs.readFile('demofile.html', function(err, data){
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        
+        res.write(data);
+        res.end();
+    });
+}).listen(8085);
