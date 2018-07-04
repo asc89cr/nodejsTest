8b3cb31a25e945a654721e6e88a8e92e224cb62d
Sandi, ArnoldX <arnoldx.sandi@intel.com>
2018-07-04 11:50:52 -0600

    edit file

--- demo_fs.js
+++ demo_fs.js
@@ -5,7 +5,6 @@ var u = require('upper-case');
 http.createServer(function (req, res){
     fs.readFile('demofile.html', function(err, data){
         res.writeHead(200, {'Content-Type': 'text/html'});
-        
         res.write(data);
         res.end();
     });
